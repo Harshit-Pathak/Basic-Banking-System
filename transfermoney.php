d28572a299f3f1306e21a253ff0af9b526c407e0
Harshit-Pathak <49994175+Harshit-Pathak@users.noreply.github.com>
2021-01-09 19:44:52 +0530

    Update transfermoney.php

--- transfermoney.php
+++ transfermoney.php
@@ -30,7 +30,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-<a class="navbar-brand" href="index1.php">TSF BANK</a>
+<a class="navbar-brand" href="index.php">TSF BANK</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
@@ -39,7 +39,7 @@
 <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
     <ul class="navbar-nav">
     <li class="nav-item">
-        <a class="nav-link" href="index1.php">Home</a>
+        <a class="nav-link" href="index.php">Home</a>
     </li>
     
     <li class="nav-item">
