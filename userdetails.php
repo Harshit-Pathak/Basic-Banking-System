36c8e7c7add67e2d1031732fe4e3d224921b0e9a
Harshit-Pathak <49994175+Harshit-Pathak@users.noreply.github.com>
2021-01-09 19:48:00 +0530

    Update userdetails.php

--- userdetails.php
+++ userdetails.php
@@ -112,7 +112,7 @@ if(isset($_POST['submit']))
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-<a class="navbar-brand" href="index1.php">TSF BANK</a>
+<a class="navbar-brand" href="index.php">TSF BANK</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
@@ -121,7 +121,7 @@ if(isset($_POST['submit']))
 <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
     <ul class="navbar-nav">
     <li class="nav-item">
-        <a class="nav-link" href="index1.php">Home</a>
+        <a class="nav-link" href="index.php">Home</a>
     </li>
     
     <li class="nav-item">
