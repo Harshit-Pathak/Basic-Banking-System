fff50a15279a4e8fdfd3c95582b58a3cc33a56db
Harshit-Pathak <49994175+Harshit-Pathak@users.noreply.github.com>
2021-01-09 19:43:31 +0530

    Update and rename index1.php to index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-     <a class="navbar-brand" href="index1.php">TSF BANK</a></img>
+     <a class="navbar-brand" href="index.php">TSF BANK</a></img>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -31,7 +31,7 @@
         <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link active" href="index1.php">Home</a>
+                <a class="nav-link active" href="index.php">Home</a>
             </li>
             
             <li class="nav-item">
