b150bd1efe7b1cb902521cb62be08ea60409da1d
Harshit-Pathak <49994175+Harshit-Pathak@users.noreply.github.com>
2021-01-09 21:28:35 +0530

    Update tsf.sql

--- sql/tsf.sql
+++ sql/tsf.sql
@@ -1,44 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 09, 2021 at 03:51 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE `transaction` (
-  `sno` int(3) NOT NULL,
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tsf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+CREATE TABLE IF NOT EXISTS `transaction` (
+  `sno` int(3) NOT NULL AUTO_INCREMENT,
   `sender` text NOT NULL,
   `receiver` text NOT NULL,
   `balance` int(8) NOT NULL,
-  `datetime` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sno`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `transaction`
+--
 
-CREATE TABLE `users` (
-  `id` int(3) NOT NULL,
+INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+(1, 'Harshit', 'Hans', 450, '2021-01-09 00:20:45'),
+(2, 'Walter', 'Harshit', 550, '2021-01-09 17:04:46'),
+(3, 'Harshit', 'Rohit', 1000, '2021-01-09 17:07:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `email` varchar(30) NOT NULL,
-  `balance` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `balance` int(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `users`
+--
 
 INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
-(1, 'Harshit', 'pathakhan@gmail.com', 50000),
-(2, 'Walter', 'white@gmail.com', 30000),
+(1, 'Harshit', 'pathak@gmail.com', 49100),
+(2, 'Walter', 'white@gmail.com', 29450),
 (3, 'Darth', 'vader@gmail.com', 40000),
 (4, 'Bruce', 'wayne@gmail.com', 50000),
-(5, 'Rohit', 'sharma@gmail.com', 40000),
+(5, 'Rohit', 'sharma@gmail.com', 41000),
 (6, 'Ravi', 'jadeja@gmail.com', 30000),
 (7, 'Light', 'yagami@gmail.com', 50000),
 (8, 'Chris', 'nolan@gmail.com', 40000),
-(9, 'Hans', 'zimmer@gmail.com', 30000),
-(10, 'Bojack', 'horseman@gmail.com', 50000)
+(9, 'Hans', 'zimmer@gmail.com', 30450),
+(10, 'Bojack', 'horseman@gmail.com', 60000);
+COMMIT;
 
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`sno`);
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `transaction`
-  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `users`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
